0741552227666cf6d17d6cce2de88196feefa5d0
Prathamesh Sahane <43199301+prathameshsahane@users.noreply.github.com>
2022-01-29 21:49:43 +0530

    Add files via upload

---
+++ ElseIf.java
@@ -0,0 +1,22 @@
+package PreBatch;
+
+public class ElseIf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=10;
+		int num2=20;
+		int num3=30;
+		
+		if(num1>=num2 && num1>=num3) {
+			System.out.println(num1);
+		}
+		else if(num2>=num2 && num2>=num3) {
+			System.out.println(num2);
+		}
+		else {
+			System.out.println(num3);
+		}
+	}
+
+}
